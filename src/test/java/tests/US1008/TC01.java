ac8de9b47567c304349dafc5a736ceb5f74d09a5
Yılmaz Tarhan <aslankral3325@gmail.com>
2022-02-13 01:00:08 +0300

    Hazırlık.

--- src/test/java/tests/US1008/TC01.java
+++ src/test/java/tests/US1008/TC01.java
@@ -2,7 +2,7 @@ package tests.US1008;
 
 public class TC01 {
     //Yönetici olarak Anasayfaya sorunsuz giriş yapabilmeliyim
-    //Yönetici olarak Anasayfaya sorunsuz giriş yapabilmeliyim
+    //Yönetici  sağ üst köşede "Log İn" sayfasını tıklamalıdır
     //Yönetici  username ve password girip "Log in" butonuna basmalıdır
 
 
