35997c512511bd2470027f51ef93d9fa4fd19a24
hüseyin <hseyinzdemir500334@gmail.com>
2022-02-13 22:16:47 +0100

    huseyin test 3 tamam

--- src/test/java/tests/US1010/TC02.java
+++ src/test/java/tests/US1010/TC02.java
@@ -1,4 +1,21 @@
 package tests.US1010;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.US1010Page;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class TC02 {
+    US1010Page us1010Page=new US1010Page();
+    @Test
+    public void roomsSayfaTesti(){
+
+        us1010Page.kullaniciGiris();
+        us1010Page.roomsButtonElementi.click();
+        Assert.assertEquals(us1010Page.roomsSayfasibaslikyazisi.getText(),ConfigReader.getProperty("HMCUserRoomsExpectText"));
+    }
+
+
+
 }
